2d28db94e1e3a8cab0703eda4af0eb1a71b858f5
karmarand <59459569+karmarand@users.noreply.github.com>
2020-01-20 10:06:58 -0500

    Add files via upload

---
+++ practice.py
@@ -0,0 +1,30 @@
+
+
+
+
+string = 'this if is you not are a reading very this good then way you to have hide done a it message wrong'
+>>> string.split()
+['this', 'if', 'is', 'you', 'not', 'are', 'a', 'reading', 'very', 'this', 'good', 'then', 'way', 'you', 'to', 'have', 'hide', 'done', 'a', 'it', 'message', 'wrong']
+>>> len(string.split())
+22
+>>> words = string.split()
+>>> for x in range(0,22,2):
+	print('%s' %(words[x]))
+
+	
+this
+is
+not
+a
+very
+good
+way
+to
+hide
+a
+message
+
+
+
+    
+
